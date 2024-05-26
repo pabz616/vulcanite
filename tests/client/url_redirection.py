28d6d24dc59eff8d03713db69b7f96d2410b06e9
Pablo Vergara <vergara.pablo0616@gmail.com>
2024-05-25 22:07:27 -0400

    tests for url redirect, pt. 2

--- tests/client/url_redirection.py
+++ tests/client/url_redirection.py
@@ -74,56 +74,63 @@ class TestForcedURLRedirect:
             expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
         
     def test_url_redirect_using_triple_backslash_to_bypass_HTTP_blacklisted_keyword(self, page: Page, target_url):
-        redirect_url = "///google.com"
-        forced_redirect_url = target_url+"?destination="+redirect_url
-        page.goto(forced_redirect_url)
-        expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
+        with allure.step("Visit site and attempt forced-redirect using triple-backslash - ///"):
+            redirect_url = "///google.com"
+            forced_redirect_url = target_url+"?destination="+redirect_url
+            page.goto(forced_redirect_url)
+            expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
         
     def test_url_redirect_using_HTTPS_to_bypass_double_forward_slash(self, page: Page, target_url):
-        redirect_url = "https:google.com"
-        forced_redirect_url = target_url+"?destination="+redirect_url
-        page.goto(forced_redirect_url)
-        expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
+        with allure.step("Visit site and attempt forced-redirect using HTTPS to bypass double-forward slash"):
+            redirect_url = "https:google.com"
+            forced_redirect_url = target_url+"?destination="+redirect_url
+            page.goto(forced_redirect_url)
+            expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
             
     def test_url_redirect_using_escape_sequence_to_bypass_double_forward_slash(self, page: Page, target_url):
-        redirect_url = "\/\/google.com/"
-        forced_redirect_url = target_url+"?destination="+redirect_url
-        page.goto(forced_redirect_url)
-        expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
+        with allure.step("Visit site and attempt forced-redirect using escape sequence to bypass double-forward slash"):
+            redirect_url = "\/\/google.com/"
+            forced_redirect_url = target_url+"?destination="+redirect_url
+            page.goto(forced_redirect_url)
+            expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
         
     def test_url_redirect_using_url_encoding_to_bypass_blacklisted_period_character(self, page: Page, target_url):
-        redirect_url = "//google%E3%80%82com"
-        forced_redirect_url = target_url+"?redir="+redirect_url
-        page.goto(forced_redirect_url)
-        expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
+        with allure.step("Visit site and attempt forced-redirect using url encoding to bypass blacklisted period character"):
+            redirect_url = "//google%E3%80%82com"
+            forced_redirect_url = target_url+"?redir="+redirect_url
+            page.goto(forced_redirect_url)
+            expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
             
     def test_url_redirect_using_null_byte_to_bypass_blacklist_filter(self, page: Page, target_url):
-        redirect_url = "//google%00.com"
-        forced_redirect_url = target_url+"?redir="+redirect_url
-        page.goto(forced_redirect_url)
-        expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
+        with allure.step("Visit site and attempt forced-redirect using null byte to bypass blacklisted filter"):
+            redirect_url = "//google%00.com"
+            forced_redirect_url = target_url+"?redir="+redirect_url
+            page.goto(forced_redirect_url)
+            expect(page).not_to_have_url(f"https:{redirect_url}"), f"Vulnerability found! Forced redirected to https:{redirect_url} was successful"
         
     def test_url_redirect_using_parameter_pollution(self, page: Page, target_url):
-        redirect_url = "?next=whitelisted.com&next=google.com"
-        forced_redirect_url = target_url+"?redir="+redirect_url
-        page.goto(forced_redirect_url)
-        expect(page).not_to_have_url(f"{redirect_url}"), f"Vulnerability found! Forced redirected to {redirect_url} was successful"
+        with allure.step("Visit site and attempt forced-redirect using parameter pollution"):
+            redirect_url = "?next=whitelisted.com&next=google.com"
+            forced_redirect_url = target_url+"?redir="+redirect_url
+            page.goto(forced_redirect_url)
+            expect(page).not_to_have_url(f"{redirect_url}"), f"Vulnerability found! Forced redirected to {redirect_url} was successful"
         
     def test_url_redirect_using_at_character(self, page: Page, target_url):
-        """browser will redirect to anything after the "@" """
+        with allure.step("Visit site and attempt forced-redirect using '@' character after redirection url"):        
+            forced_redirect_url = redirect_url+"@hacker.com"
+            page.goto(forced_redirect_url)
+            expect(page).not_to_have_url(f"{forced_redirect_url}"), f"Vulnerability found! Forced redirected to {redirect_url} was successful"
         
-        forced_redirect_url = redirect_url+"@demoqa.com"
-        page.goto(forced_redirect_url)
-        expect(page).not_to_have_url(f"{DemoQA}"), f"Vulnerability found! Forced redirected to {redirect_url} was successful"
-        
-    def test_url_redirect_creating_folder_as_their_domain(self, page: Page, target_url):    
-        forced_redirect_url = f"{target_url}/{redirect_url}"
-        page.goto(forced_redirect_url)
-        expect(page).not_to_have_url(f"{redirect_url}"), f"Vulnerability found! Forced redirected to {redirect_url} was successful"
+    def test_url_redirect_creating_folder_as_their_domain(self, page: Page, target_url):
+        with allure.step("Visit site and attempt forced-redirect by creating a folder as the domain - url_a/url_b"):    
+            forced_redirect_url = f"{target_url}/{redirect_url}"
+            page.goto(forced_redirect_url)
+            expect(page).not_to_have_url(f"{redirect_url}"), f"Vulnerability found! Forced redirected to {redirect_url} was successful"
     
     @pytest.mark.xfail(reason="page fails to load, but url loads with redirect parameter")    
     def test_url_redirect_using_xss_from_data_wrapper(self, page: Page, target_url):
-        forced_redirect_url = f"{target_url}/redirect.php?url=data:text/html;base64,PHNjcmlwdD5hbGVydCgiWFNTIik7PC9zY3JpcHQ+Cg=="
-        page.goto(forced_redirect_url)
-        expect(page).not_to_have_url(f"{forced_redirect_url}"), f"Vulnerability found! Forced redirected to {forced_redirect_url} was successful"
+        with allure.step("Visit site and attempt forced-redirect using cross-site-script from data-wrapper"):
+            forced_redirect_url = f"{target_url}/redirect.php?url=data:text/html;base64,PHNjcmlwdD5hbGVydCgiWFNTIik7PC9zY3JpcHQ+Cg=="
+            page.goto(forced_redirect_url)
+            expect(page).not_to_have_url(f"{forced_redirect_url}"), f"Vulnerability found! Forced redirected to {forced_redirect_url} was successful"
 
